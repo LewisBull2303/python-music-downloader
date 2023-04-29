b2097509876a2b270e263c3eecf8d63cb6afaabb
LewisBull2303 <129514763+LewisBull2303@users.noreply.github.com>
2023-04-29 12:12:36 +0100

    Add files via upload

--- youtube_music_downloader/main.py
+++ youtube_music_downloader/main.py
@@ -14,6 +14,4 @@ os.rename(out_file, new_file)
 
 print(yt.title + " has been successfully downloaded.")
 
-'''MTEwMDM4MjkwMDk0NTQyODU5MQ.GN1nCK.cBt5N3r8hzpxKQWe2lCmAuyqHU76XT6DGyOZd0'''
 
-'''https://discord.com/api/oauth2/authorize?client_id=1100382900945428591&permissions=8&scope=bot'''
